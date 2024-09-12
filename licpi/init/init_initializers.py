1f8f5235e3b6537ffcc15957c3d8da1a89ee7911
Ryo Nakamura <ryo.nakamura.nt@gmail.com>
2024-09-12 11:03:55 +0800

    add initializers

---
+++ licpi/init/init_initializers.py
@@ -0,0 +1,21 @@
+import math
+from .init_basic import *
+
+
+def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
+    a = gain * math.sqrt(6 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
+
+def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
+    std = gain * math.sqrt(2 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
+
+def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
+    assert nonlinearity == "relu", "Only relu supported currently"
+    bound = math.sqrt(6 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
+
+def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
+    assert nonlinearity == "relu", "Only relu supported currently"
+    std = math.sqrt(2 / fan_in)
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
