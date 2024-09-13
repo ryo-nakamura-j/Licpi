a8518cf0df51b36c516edb1bf04282d8fa5393ac
Ryo Nakamura <ryo.nakamura.nt@gmail.com>
2024-09-13 16:03:36 +0800

    add nn

---
+++ nn/nn_basic.py
@@ -0,0 +1,95 @@
+from typing import List, Callable, Any
+from licpi.autograd import Tensor
+from licpi import ops
+import licpi.init as init
+import numpy as np
+
+class Parameter(Tensor):
+    """A special kind of tensor that represents parameters."""
+
+
+def _unpack_params(value: object) -> List[Tensor]:
+    if isinstance(value, Parameter):
+        return [value]
+    elif isinstance(value, Module):
+        return value.parameters()
+    elif isinstance(value, dict):
+        params = []
+        for k, v in value.items():
+            params += _unpack_params(v)
+        return params
+    elif isinstance(value, (list, tuple)):
+        params = []
+        for v in value:
+            params += _unpack_params(v)
+        return params
+    else:
+        return []
+
+def _child_modules(value: object) -> List["Module"]:
+    if isinstance(value, Module):
+        modules = [value]
+        modules.extend(_child_modules(value.__dict__))
+        return modules
+    if isinstance(value, dict):
+        modules = []
+        for k, v in value.items():
+            modules += _child_modules(v)
+        return modules
+    elif isinstance(value, (list, tuple)):
+        modules = []
+        for v in value:
+            modules += _child_modules(v)
+        return modules
+    else:
+        return []
+
+class Module:
+    def __init__(self):
+        self.training = True
+
+    def parameters(self) -> List[Tensor]:
+        """Return the list of parameters in the module."""
+        return _unpack_params(self.__dict__)
+
+    def _children(self) -> List["Module"]:
+        return _child_modules(self.__dict__)
+
+    def eval(self):
+        self.training = False
+        for m in self._children():
+            m.training = False
+
+    def train(self):
+        self.training = True
+        for m in self._children():
+            m.training = True
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+
+
+class Identity(Module):
+    def forward(self, x):
+        return x
+
+
+class Linear(Module):
+    def __init__(self, in_features, out_features, bias=True, device=None, dtype="float32"):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        ### BEGIN YOUR SOLUTION
+        self.weight = Parameter(init.kaiming_uniform(in_features, out_features, requires_grad=True))
+        self.bias = Parameter(init.kaiming_uniform(out_features, 1, requires_grad=True).transpose()) if bias else None
+        ### END YOUR SOLUTION
+
+    def forward(self, X: Tensor) -> Tensor:
+        ### BEGIN YOUR SOLUTION
+        # X: N x in_feat, weight: in_feat x out_feat
+        out = X.matmul(self.weight)
+        if self.bias:
+            out += self.bias.broadcast_to(out.shape)
+        return out
+        ### END YOUR SOLUTION
