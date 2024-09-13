1be18b4268b819e6e2460ae755c4288261e0babc
Ryo Nakamura <ryo.nakamura.nt@gmail.com>
2024-09-13 16:04:01 +0800

    add logsumexp

--- licpi/ops.py
+++ licpi/ops.py
@@ -299,3 +299,34 @@ class ReLU(TensorOp):
 
 def relu(a):
     return ReLU()(a)
+
+
+class LogSumExp(TensorOp):
+    def __init__(self, axes: Optional[tuple] = None):
+        self.axes = axes
+
+    def compute(self, Z):
+        ### BEGIN YOUR SOLUTION
+        max_z_original = array_api.max(Z, self.axes, keepdims=True) 
+        max_z_reduce = array_api.max(Z, self.axes)
+        return array_api.log(array_api.sum(array_api.exp(Z - max_z_original), self.axes)) + max_z_reduce 
+        ### END YOUR SOLUTION
+
+    def gradient(self, out_grad, node):
+        ### BEGIN YOUR SOLUTION
+        z = node.inputs[0]
+        max_z = z.realize_cached_data().max(self.axes, keepdims=True)
+        exp_z = exp(z - max_z)
+        sum_exp_z = summation(exp_z, self.axes)
+        grad_sum_exp_z = out_grad / sum_exp_z
+        expand_shape = list(z.shape)
+        axes = range(len(expand_shape)) if self.axes is None else self.axes
+        for axis in axes:
+            expand_shape[axis] = 1
+        grad_exp_z = grad_sum_exp_z.reshape(expand_shape).broadcast_to(z.shape)
+        return grad_exp_z * exp_z
+        ### END YOUR SOLUTION
+
+
+def logsumexp(a, axes=None):
+    return LogSumExp(axes=axes)(a)
